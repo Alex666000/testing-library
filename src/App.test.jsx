4e023dc91cd4be0628771edd1f0f90bc3e2eb495
Aleksandr_UB <bogdanov777000@mail.ru>
2023-04-08 18:06:49 +0300

    mock тестирование асинхронных запросов

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,83 +1,56 @@
 import React from "react";
-import {render, screen, fireEvent} from "@testing-library/react";
+import axios from "axios";
+import { render, screen, act } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
 
+// мокаем стороннии модуль аксиос
+// мокаем функции с помощью которого осуществляется запрос, потом добавляем в стаб данные ответа
+// то есть  сами конструируем ответ от сервиса и проверяем состояние нашего компонента или результат отрисовки
+// начинаем с подмены данных - мокаем аксиос
+jest.mock("axios");
+const hits = [
+    { objectID: "1", title: "Angular" },
+    { objectID: "2", title: "React" },
+];
+
 describe("App", () => {
-    test("renders App component", async () => {
-        render(<App/>);
-        await screen.findByText(/Logged in as/);
-        expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
-        // fireEvent.change(screen.getByRole("textbox"), {
-        //   target: { value: "React" },
-        // });
-
-        // находит нужный инпут и вводит туда текст -- текст передается напрямую
-        userEvent.type(screen.getByRole("textbox"), "React");
-        expect(screen.getByText(/Searches for React/)).toBeInTheDocument();
-    });
-});
-
-describe("events", () => {
-    it("checkbox click", () => {
-        // ссылка на ДОМ элемент инпут
-        const {container} = render(<input type="checkbox"/>);
-        const checkbox = container.firstChild;
-        expect(checkbox).not.toBeChecked();
-        // fireEvent.click(checkbox);
-        userEvent.click(checkbox);
-        expect(checkbox).toBeChecked();
-    });
-
-    it("double click", () => {
-        const onChange = jest.fn();
-        const {container} = render(<input type="checkbox" onChange={onChange}/>);
-        const checkbox = container.firstChild;
-        userEvent.dblClick(checkbox);
-        expect(onChange).toHaveBeenCalledTimes(2);
-    });
-
-    it("tab(перемещение по интерфейсу табом)", () => {
-        render(
-            <div>
-                <input data-testid="element" type="checkbox"/>
-                <input data-testid="element" type="radio"/>
-                <input data-testid="element" type="number"/>
-            </div>
+    it("fetches news from an API", async () => {
+        // в замоканном аксиос переписываем реализацию метода get - mockImplementationOnce() -- сюда передаем функцию возвращающую промисс
+        axios.get.mockImplementationOnce(() => Promise.resolve({ data: { hits } }));
+        render(<App />);
+        // нажимаем на кнопку
+        userEvent.click(screen.getByRole("button"));
+        // получаем данные и ищем в разметки наш список
+        const items = await screen.findAllByRole("listitem");
+        expect(items).toHaveLength(2);
+        expect(axios.get).toHaveBeenCalledTimes(1);
+        expect(axios.get).toHaveBeenCalledWith(
+            "http://hn.algolia.com/api/v1/search?query=React"
         );
-        // находим все поля
-        const [checkbox, radio, number] = screen.getAllByTestId("element");
-        // имулируем нажатие таб
-        userEvent.tab();
-        // идем по порядку сверху вниз
-        expect(checkbox).toHaveFocus();
-        userEvent.tab();
-        expect(radio).toHaveFocus();
-        userEvent.tab();
-        expect(number).toHaveFocus();
     });
-
-    it("select", () => {
-        render(
-            <select>
-                <option value="1">A</option>
-                <option value="2">B</option>
-                <option value="3">C</option>
-            </select>
-        );
-// нашли селект и имитируем выбор его
-        userEvent.selectOptions(screen.getByRole("combobox"), "1");
-        expect(screen.getByText("A").selected).toBeTruthy();
-        // роль элемента селект это: combobox - второй аргумент это значение которое пользователь выбирает
-        userEvent.selectOptions(screen.getByRole("combobox"), "2");
-        // опция "B" выбрана
-        expect(screen.getByText("B").selected).toBeTruthy();
-        // с опции А" выбор снят
-        expect(screen.getByText("A").selected).toBeFalsy();
+// негативный ответ:
+    it("fetches news from an API and reject", async () => {
+        // Promise.reject
+        axios.get.mockImplementationOnce(() => Promise.reject(new Error()));
+        render(<App />);
+        userEvent.click(screen.getByRole("button"));
+        // поиск ошибки
+        const message = await screen.findByText(/Something went wrong/);
+        expect(message).toBeInTheDocument();
+    });
+// так можно ожидать выполнение промисс более явно
+    it("fetches news from an API (alternative)", async () => {
+        const promise = Promise.resolve({ data: { hits } });
+        axios.get.mockImplementationOnce(() => promise);
+        render(<App />);
+        userEvent.click(screen.getByRole('button'));
+        // обертка act
+        await act(() => promise);
+        expect(screen.getAllByRole('listitem')).toHaveLength(2);
     });
 });
 /*
-userEvent - имитируем реальное поведение браузера более точно и подробнее fireEvent-та -- реальное взаимодецствие пользователя и интерфейса
+переопределение логики внешних модулей например:     axios.get.mockImplementationOnce(() => Promise.reject(new Error()));
 
-getByRole("combobox") - роль если не знаем элемента то гуглим...
  */
