6f73455e126eb3fb7b1f5a3286ffd335581fb89f
Aleksandr_UB <bogdanov777000@mail.ru>
2023-04-08 15:44:45 +0300

    начало render screen

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import {render, screen} from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+    render(<App/>);
+    // ищем элемент по тексту через регулярку
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+
+//отрисуем компонент Арр
+    render(<App/>);
+    //  запускаем тест и увидем полную разметку нашего приложения
+    screen.debug();
 });
+
+/*
+Используя реакт тестинг library - получим реальный доступ к ДОМ узлам
+
+
+ */
+
+
+
