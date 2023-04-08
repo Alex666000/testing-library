ca87b444f00e2b6abe58fadd114c23d05d287cd0
Aleksandr_UB <bogdanov777000@mail.ru>
2023-04-08 17:51:48 +0300

    userEvent

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,60 +1,83 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import {render, screen, fireEvent} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 describe("App", () => {
-    // async - так как делаем запрос в коде
     test("renders App component", async () => {
-        // отрисовываем
-        render(<App />);
-        // проверяем разметку
+        render(<App/>);
         await screen.findByText(/Logged in as/);
-        // изначально строки /Searches for JavaScript/ нету,
         expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
-        // внутрь передаем найденный элемент и событие - после события изменения поля она появляется строка в документе стр 18
-        fireEvent.change(screen.getByRole("textbox"), {
-            target: { value: "React" },
-        });
+        // fireEvent.change(screen.getByRole("textbox"), {
+        //   target: { value: "React" },
+        // });
+
+        // находит нужный инпут и вводит туда текст -- текст передается напрямую
+        userEvent.type(screen.getByRole("textbox"), "React");
         expect(screen.getByText(/Searches for React/)).toBeInTheDocument();
     });
 });
 
 describe("events", () => {
-    it("checkbox click(клик по чекбоксу)", () => {
-        // jest.fn() - нужная функция -- из результата работы функции render()  --- вместо screen используем теперь...
-        const handleChange = jest.fn();
-        // container  вернет ссылку на Дом элемент куда вмонтирован наш компонент
-        const { container } = render(
-            <input type="checkbox" onChange={handleChange} />
-        );
-        // получили checkbox
-        // чтобы обратится к компоненту используем firstChild (можно было воспользоваться и getByRole...получили доступ к чекбоксу)
+    it("checkbox click", () => {
+        // ссылка на ДОМ элемент инпут
+        const {container} = render(<input type="checkbox"/>);
         const checkbox = container.firstChild;
-        // изначально поле не выбрано
         expect(checkbox).not.toBeChecked();
-        fireEvent.click(checkbox);
-        // слежение за функцией handleChange - что после клика она вызвана
-        expect(handleChange).toHaveBeenCalledTimes(1);
-        // чекбокс выбран
+        // fireEvent.click(checkbox);
+        userEvent.click(checkbox);
         expect(checkbox).toBeChecked();
     });
 
-    it("input focus(тестируем фокус элемента)", () => {
-        // с помощью этого атрибута data-testid ищем элементы в разметке осуществляется операция с помощью метода getByTestId
-        render(<input data-testid="simple-input" type="text" />);
-        const input = screen.getByTestId("simple-input");
-        // проверяем начальное состояние не в фокусе
-        expect(input).not.toHaveFocus();
-        // выставляем фокус
-        input.focus();
-        // элемент в фокусе?
-        expect(input).toHaveFocus();
+    it("double click", () => {
+        const onChange = jest.fn();
+        const {container} = render(<input type="checkbox" onChange={onChange}/>);
+        const checkbox = container.firstChild;
+        userEvent.dblClick(checkbox);
+        expect(onChange).toHaveBeenCalledTimes(2);
+    });
+
+    it("tab(перемещение по интерфейсу табом)", () => {
+        render(
+            <div>
+                <input data-testid="element" type="checkbox"/>
+                <input data-testid="element" type="radio"/>
+                <input data-testid="element" type="number"/>
+            </div>
+        );
+        // находим все поля
+        const [checkbox, radio, number] = screen.getAllByTestId("element");
+        // имулируем нажатие таб
+        userEvent.tab();
+        // идем по порядку сверху вниз
+        expect(checkbox).toHaveFocus();
+        userEvent.tab();
+        expect(radio).toHaveFocus();
+        userEvent.tab();
+        expect(number).toHaveFocus();
+    });
+
+    it("select", () => {
+        render(
+            <select>
+                <option value="1">A</option>
+                <option value="2">B</option>
+                <option value="3">C</option>
+            </select>
+        );
+// нашли селект и имитируем выбор его
+        userEvent.selectOptions(screen.getByRole("combobox"), "1");
+        expect(screen.getByText("A").selected).toBeTruthy();
+        // роль элемента селект это: combobox - второй аргумент это значение которое пользователь выбирает
+        userEvent.selectOptions(screen.getByRole("combobox"), "2");
+        // опция "B" выбрана
+        expect(screen.getByText("B").selected).toBeTruthy();
+        // с опции А" выбор снят
+        expect(screen.getByText("A").selected).toBeFalsy();
     });
 });
 /*
-реальные взаимодействия пользователей - если вводить что то в инпут компонент перерисовывается..новое значение должно отобразаться
-для имитации  пользователя с нашим интерфыейсом fireEvent()
+userEvent - имитируем реальное поведение браузера более точно и подробнее fireEvent-та -- реальное взаимодецствие пользователя и интерфейса
 
-Алгоритм: находим элемент -- имулируем событие -- проверяем отклик на событие --> итогом проверяем итоговою разметку
-
+getByRole("combobox") - роль если не знаем элемента то гуглим...
  */
